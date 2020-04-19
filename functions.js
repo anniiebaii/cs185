f5439a3f4f013c98d8722932d1412803f4aabaf3
Annie Bai <anbaii98@gmail.com>
2020-04-18 23:19:03 -0700

    fix sub-page container margins

--- functions.js
+++ functions.js
@@ -1,3 +1,6 @@
+/* Back to the Top Functions */
+
+// Scroll Detection
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     mybutton.style.display = "block";
@@ -6,8 +9,29 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// Scroll back to the top 
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+
+/* Modal Functions */
+function openModal(id)  {
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById(id);
+    var modalImg = document.getElementById("modal-content");
+    var captionText = document.getElementById("caption");
+
+    modal.style.display = "block";
+    modalImg.src = img.src; // insert clicked image into modal content
+    captionText.innerHTML = img.alt; // alt as caption
+
+    // <span> element for "x" button to close the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
 }
