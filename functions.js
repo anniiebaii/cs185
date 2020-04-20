3a8588d5f35afc5f1b57748d2c5fa5f885a80e5d
Annie Bai <anbaii98@gmail.com>
2020-04-20 15:02:05 -0700

    fix back to top showing during modal bug

--- functions.js
+++ functions.js
@@ -2,14 +2,16 @@
 
 // Scroll Detection
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    var modal = document.getElementById("myModal");
+  if ((document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
+      && modal.style.display !== "block") {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
 
-// Scroll back to the top 
+// Scroll back to the top
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
@@ -24,6 +26,7 @@ function openModal(id)  {
     var captionText = document.getElementById("caption");
 
     modal.style.display = "block";
+    mybutton.style.display = "none";
     modalImg.src = img.src; // insert clicked image into modal content
     captionText.innerHTML = img.alt; // alt as caption
 
@@ -33,5 +36,6 @@ function openModal(id)  {
     // close the modal
     span.onclick = function() {
       modal.style.display = "none";
+      scrollFunction();
     }
 }
